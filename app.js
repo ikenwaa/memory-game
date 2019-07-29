b235a09ee6e8477e9aebdc539f486c0ffc124c52
Augustine Ikenwa <ajohn9353@gmail.com>
2019-07-29 18:16:31 +0100

    new edit

--- app.js
+++ app.js
@@ -1,51 +1,78 @@
-// const shuffle = function(array) {
+// const empty = document.getElementsByClassName('fab');
+// const cards = document.getElementsByClassName('card');
+// const btn = document.getElementById('refresh');
 
-// 	var currentIndex = array.length;
-// 	var temporaryValue, randomIndex;
+const cards = document.querySelectorAll('.card');
 
-// 	// While there remain elements to shuffle...
-// 	while (0 !== currentIndex) {
-// 		// Pick a remaining element...
-// 		randomIndex = Math.floor(Math.random() * currentIndex);
-// 		currentIndex -= 1;
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
-// 		// And swap it with the current element.
-// 		temporaryValue = array[currentIndex];
-// 		array[currentIndex] = array[randomIndex];
-// 		array[randomIndex] = temporaryValue;
-// 	}
 
-// 	return array;
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-// };
+  this.classList.add('flip');
 
-// const array=["fa-html5", "fa-js","fa-css3-alt", "fa-react", "fa-node","fa-angular","fa-git","fa-vuejs", "fa-html5", "fa-js","fa-css3-alt","fa-node","fa-angular","fa-git","fa-vuejs", "fa-react"];
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
 
-// const shuffleCards = (e) => {
-//   const results = shuffle(array);
-//   const a = results.map((result => {
-//     return `<div class="card" type="html"><i class="fab ${result}"></i></div>`;
-//   })).join(' ');
-//   document.getElementById('board').innerHTML = a;
-// };
-
-// window.onload = shuffleCards();
-window.onload = () => {
-
+    return;
+  }
+  secondCard = this;
+  checkForMatch();
 }
-const empty = document.getElementsByClassName('fab');
-const cards = document.getElementsByClassName('card');
-const btn = document.getElementById('refresh');
+
+checkForMatch = () => {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+  resetBoard();
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
 
 // Set cards display to none
-const startGame = () =>{
-	Array.from(empty).forEach((empty) => {
-		empty.style.display = 'none';
-	})
-}
-setTimeout(startGame, 5000);
+// const startGame = () =>{
+// 	Array.from(empty).forEach((empty) => {
+// 		empty.style.display = 'none';
+// 	});
+// }
+// setTimeout(startGame, 3000);
 
-// Shuffle Cards Positions
+// // Shuffle Cards Positions
 const shuffle = () =>{
 	Array.from(cards).forEach((card) =>{
 		card.style.order = Math.floor(Math.random() * 16);
@@ -54,25 +81,14 @@ const shuffle = () =>{
 	brown.style.display = 'block';
 	let count = document.getElementById('moves');
 	count.textContent = "0"; 
-	})
-	console.log('Card has reshuffled.')
-	setTimeout(startGame, 5000);
+	});
+	setTimeout(startGame, 3000);
 }
 
 shuffle();
 
 // Count Number of Moves
-const counter = () => {
+const counter = function counter(){
 	let count = document.getElementById('moves');
 	count.textContent++; 
 }
-
-// Display cards on click
-function click(){
-	Array.from(cards).forEach(function(card){
-		card.addEventListener("click", function(e){
-			this.childNodes[0].style.display = 'block';
-		});
-	});
-}
-click();
